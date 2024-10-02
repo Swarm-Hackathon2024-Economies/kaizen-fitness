e0669dc855bdf93a753542b8fbc7fa4bd88ef03e
Naoya Hashidate <naoya_hashidate@mail.toyota.co.jp>
2024-10-02 19:56:16 +0900

    デバイスに応じて画面を分けるようにした＆ディレクトリ構造のリファクタ
    Co-authored-by: Shion Koga <shion_koga@mail.toyota.co.jp>

--- kaizen-fitness/kaizen-fitness/ContentView.swift
+++ kaizen-fitness/kaizen-fitness/ContentView.swift
@@ -1,46 +1,12 @@
-//
-//  ContentView.swift
-//  kaizen-fitness
-//
-//  Created by takuya on 9/12/24.
-//
-
 import SwiftUI
 import MapKit
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            SelectPlanScreen()
-                .tabItem {
-                    VStack {
-                        Image(systemName: "filemenu.and.selection")
-                        Text("Plan")
-                    }
-                }
-            DriveScreen(gifName: "car", minimumInterval: 0.05)
-                .tabItem {
-                    VStack {
-                        Image(systemName: "car.fill")
-                        Text("Drive")
-                    }
-                }
-            MapView()
-                .tabItem {
-                    VStack {
-                        Image(systemName: "flag.fill")
-                        Text("test")
-                    }
-                }
-            VideoPlayView(videoID: "hge3fr50o0o")
-                .cornerRadius(12)
-                .shadow(radius: 5)
-                .tabItem {
-                    VStack {
-                        Image(systemName: "movieclapper")
-                        Text("Video")
-                    }
-                }
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            IOSHomeScreen()
+        } else if UIDevice.current.userInterfaceIdiom == .pad {
+            SelectPlanView()
         }
     }
 }
---
+++ kaizen-fitness/kaizen-fitness/iPhone/IOSHomeScreen.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct IOSHomeScreen: View {
+    var body: some View {
+        Text("iOS Home")
+    }
+}
+
+#Preview {
+    IOSHomeScreen()
+}
